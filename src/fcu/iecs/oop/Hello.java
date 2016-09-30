48e248801a4690828fc5ad5ce236654390a61e05
user <user@FD09>
2016-09-30 09:23:06 +0800

    修改World

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
 	public static void main(String[] args) {
 				
-				System.out.println("HelloWorld");
+				System.out.println("HelloWorld OOP");
 		// TODO Auto-generated method stub
 
 	}
