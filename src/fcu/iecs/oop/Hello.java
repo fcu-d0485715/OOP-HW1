050515d0c0f62ef35b7415c38985a7929bb45ea3
user <user@FD09>
2016-09-30 09:10:16 +0800

    A

---
+++ src/fcu/iecs/oop/Hello.java
@@ -0,0 +1,12 @@
+package fcu.iecs.oop;
+
+public class Hello {
+
+	public static void main(String[] args) {
+				
+				System.out.println("HelloWorld");
+		// TODO Auto-generated method stub
+
+	}
+
+}
